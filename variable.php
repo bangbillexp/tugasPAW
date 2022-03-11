5056958129124aae03271e726e43faf7c231bf0d
billy <billymorgensimbolon@gmail.com>
2022-03-11 09:22:33 +0700

    Nomor 2 (variable)

---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+    echo" DIKETAHUI : <br>a = 7 <br> b = 5 <br> c = 3 <br> d = 1 <br>";
+    $a = 7 ;
+    $b = 5 ;
+    $c = 3 ;
+    $d = 1 ;
+    $e = $a + $d;
+    $f = $b * $c;
+    $g = ($a * $c) + ($b * $d) ;
+    echo "Hasil penjumlahan a + d adalah = $e<br>
+    Hasil perkalian b * c adalah = $f<br>
+    Hasil dari (a * c) + (b * d) adalah = $g";
+   
+
+?>
