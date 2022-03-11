fa41691b084c38d03305f45030a1371df26093e5
billy <billymorgensimbolon@gmail.com>
2022-03-11 09:26:49 +0700

    Nomor 3 (Fungsi)

---
+++ fungsi.php
@@ -0,0 +1,19 @@
+<?php 
+    function medan(){
+        echo "Medan adalah tempat dimana saya tinggal dari kecil hingga besar<br>" ;
+    }
+    function wisata($a,$b){
+        echo "Tempat wisata dimedan banyak sekali seperti $a dan $b . saya sangat menyukai keduanya<br>";
+    }
+    function makanan($x,$y){
+        echo "Makanan khas medan ada banyak. Namun yang paling saya suka adalah $x dan $y <br>";
+    }
+    medan();
+    $a = "Istana Maimun";
+    $b = "Pantai Cermin";
+    wisata($a,$b);
+    $x = "cimpa";
+    $y = "cipera";
+    makanan($x,$y);
+    //Latihan 3 PAW
+?>
